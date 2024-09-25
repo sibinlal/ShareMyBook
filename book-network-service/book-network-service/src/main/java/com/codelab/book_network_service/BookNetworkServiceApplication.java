3d582f1a18cf4edc0f6dae7b7655ebcc9c437d1d
sibinlal <sbnlalpnr@gmail.com>
2024-09-26 00:13:37 +0530

    book network back end service setup

---
+++ book-network-service/book-network-service/src/main/java/com/codelab/book_network_service/BookNetworkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.book_network_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkServiceApplication.class, args);
+	}
+
+}
